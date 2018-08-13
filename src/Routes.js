0dacf963b3596f06e6775a257ebe63f4aed7bfaa
Himanshu Gupta <himanshu.gupta@treebohotels.com>
2018-08-13 08:50:44 +0530

    remove route

--- src/Routes.js
+++ src/Routes.js
@@ -10,14 +10,6 @@ const GoalAdd = asyncComponent(() =>
     import('./Views/GoalAdd/GoalAdd').then(module => module.default)
 );
 
-const GoalUpdate = asyncComponent(() =>
-    import('./Views/GoalUpdate/GoalUpdate').then(module => module.default)
-);
-
-const GoalDelete = asyncComponent(() =>
-    import('./Views/GoalDelete/GoalDelete').then(module => module.default)
-);
-
 class Routes extends Component {
   render() {
     return (
@@ -25,8 +17,6 @@ class Routes extends Component {
         <React.Fragment>
           <Route exact path="/" component={GoalList} />
           <Route exact path="/goaladd/" component={GoalAdd} />
-          <Route exact path="/goalupdate/:id" component={GoalUpdate} />
-          <Route exact path="/goaldelete/:id" component={GoalDelete} />
         </React.Fragment>
       </Router>
     );
